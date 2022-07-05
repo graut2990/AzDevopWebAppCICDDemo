f7c5585e272cd310afe0cb8a20c0953693b3a9d3
Gaurav Raut <gaurav.raut@emerson.com>
2022-07-05 13:23:42 +0530

    Unit Test Project Added

---
+++ WepApp.UnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WepApp.UnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WepApp.UnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("324bc09e-bb9f-4919-96f7-4b3a67fc7a5f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WepApp.UnitTestProject/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WepApp.UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void UnitTestShouldAlwaysWorkTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void OnePlusTwoEqualThreeTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
